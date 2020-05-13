49c4ab90e7eda50061d1703a4b7b6696374f65a5
aranhalucas1 <62998019+aranhalucas1@users.noreply.github.com>
2020-05-13 16:29:32 -0300

    Category

--- src/main/java/com/estudando/course/config/TestConfig.java
+++ src/main/java/com/estudando/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.estudando.course.entities.Category;
 import com.estudando.course.entities.Order;
 import com.estudando.course.entities.User;
 import com.estudando.course.entities.enums.OrderStatus;
+import com.estudando.course.repositories.CategoryRepository;
 import com.estudando.course.repositories.OrderRepository;
 import com.estudando.course.repositories.UserRepository;
 
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Lucas Aranha", "lucas@gmail.com", "2121221", "7654321");
 		User u2 = new User(null, "Renato Aranha", "renato@gmail.com", "7657575", "126347"); 
 		
