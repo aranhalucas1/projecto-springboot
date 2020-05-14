9f0f6fb235a750295c08913af1dcd9f29b73ad43
aranhalucas1 <62998019+aranhalucas1@users.noreply.github.com>
2020-05-14 15:01:28 -0300

    Product

---
+++ src/main/java/com/estudando/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.estudando.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudando.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
