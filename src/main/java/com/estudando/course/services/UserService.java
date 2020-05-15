b1cb75d9be6e847308a99dbd42d401a05c7b1673
aranhalucas1 <62998019+aranhalucas1@users.noreply.github.com>
2020-05-15 17:47:32 -0300

    Exception handling - delete

--- src/main/java/com/estudando/course/services/UserService.java
+++ src/main/java/com/estudando/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.estudando.course.entities.User;
 import com.estudando.course.repositories.UserRepository;
+import com.estudando.course.services.exceptions.DatabaseException;
 import com.estudando.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -22,7 +25,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); 
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -30,14 +33,20 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-		
+
 	}
 
 	private void updateData(User entity, User obj) {
